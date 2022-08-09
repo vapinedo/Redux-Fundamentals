fee2e7cadce330e790b7c01e5976a545e40842f3
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-08-09 16:35:19 -0500

    [add] actions to reducer

--- app.ts
+++ app.ts
@@ -1,22 +1,35 @@
-// Actions
 interface Action {
-    type: string ;
-    payload?: any;
+  type: string;
+  payload?: any;
 }
 
-// const incrementAction: Action = {
-//     type: "INCREMENT"
-// };
-
 function reducer(state = 10, action: Action) {
-    if (action.type == "INCREMENT") {
-        return state = state + 1;
-    }
-    return state;
+  switch (action.type) {
+    case "DECREMENT":
+      return (state = state - 1);
+    case "INCREMENT":
+      return (state = state + 1);
+    case "MULTIPLY":
+        return state = state * action.payload
+    default:
+        return state;
+  }
 }
 
 // Use the reducer
-const incrementAction: Action = {
-    type: "INCREMENT"
+const decrementAction: Action = {
+  type: "DECREMENT",
 };
+const incrementAction: Action = {
+  type: "INCREMENT",
+};
+function multiplyAction(payload: any) {
+    const action: Action = {
+      type: "MULTIPLY",
+      payload,
+    };
+    return action;
+}
 console.log(reducer(10, incrementAction));
+console.log(reducer(10, decrementAction));
+console.log(reducer(10, multiplyAction(10)));
